1583d3dccf2ee9fb22e18ed3f8f030b49ea94f5d
Elyor <elyor@outlook.com>
2018-12-19 18:20:17 +0500

    First commit

---
+++ TinyMCE_UploadiFive/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TinyMCE_UploadiFive.Controllers
+{
+  public class HomeController : Controller
+  {
+
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    [ValidateInput(false)]
+    [ValidateAntiForgeryToken]
+    public ActionResult Index(FormCollection form)
+    {
+      TempData["content"] = form["content"];
+
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public ContentResult Upload(IList<HttpPostedFileBase> files)
+    {
+      var fileName = "";
+      var path = "~/upload/";
+
+      foreach (var file in files)
+      {
+        if (file == null)
+          continue;
+
+        fileName = Path.GetFileName(file.FileName);
+        file.SaveAs(Server.MapPath(path) + fileName);
+      }
+
+      return Content(Url.Content(path + fileName));
+    }
+
+  }
+}
